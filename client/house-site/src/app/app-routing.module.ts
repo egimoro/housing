8fad6b1bb130a2fd3a523cf90cde97daabd4fd09
gimoroemma <gimoremma@tutanota.com>
2021-03-06 18:32:36 +0000

    Corrected routing

--- client/house-site/src/app/app-routing.module.ts
+++ client/house-site/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HousesComponent} from './houses/houses.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'houses/:id', component: HouseDetailComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'houses', component: HousesComponent} 
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/house-site/src/app/app.module.ts
+++ client/house-site/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent} from './dashboard/dashboard.component';
+import { HousesComponent} from './houses/houses.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
+import { MessagesComponent  } from './messages/messages.component';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseDetailComponent,
+    HousesComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
